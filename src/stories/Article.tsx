639b7345ffc928048d9d5d01e33f07606c719042
Fredrik Larsson <fredrik.larsson@adeprimo.se>
2022-04-10 23:18:56 +0200

    add some small basic css

--- src/stories/Article.tsx
+++ src/stories/Article.tsx
@@ -3,12 +3,21 @@ import { MainHeading } from './components/main-heading/main-heading';
 import { Preamble } from './components/preamble/preamble';
 import { Paragraph } from './components/paragraph/paragraph';
 import paragraphs from './components/paragraph/exampleData';
+import { Figure } from './components/figure/Figure';
+import placeHolderLandscape from './assets/sora-sagano-tlCzX-8iwx4-unsplash.jpg';
 
 export const Article = () => {
+	const image = {
+		src: placeHolderLandscape,
+		alt: 'Photo of mountain',
+		width: 1920,
+		height: 1081,
+	};
 	return (
-		<article>
+		<article className='full-article full-article--center'>
 			<MainHeading />
 			<Preamble />
+			<Figure image={image} />
 			<Paragraph paragraphs={paragraphs} />
 		</article>
 	);
